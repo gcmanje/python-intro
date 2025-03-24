e99e5b4a8b67464d452df195b9a826d30c6ff633
zora <zrnaumo@gmail.com>
2025-03-24 06:44:07 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,87 @@
+from math import sqrt
+
+from Variables import result
+
+
+def greet():
+    print("Hello, functions")
+
+
+def area_circle(radius):
+    result= 22/7 * radius ** 2
+    result= round(result, 2)
+    print(result)
+
+def volume_cylinder(h,r, precision=2):
+    v= 22/7 * r ** 2 * h
+    v= round(v, precision)
+    print(f" Radius is {r}, Height is {h} ,volume is {v}")
+
+def area_of_triangle(a,b,c):
+    """"calculates the area of the triangle then returns the result"""
+    s=(a+b+c)/2
+    A=sqrt(s*(s-a)*(s-b)*(s-c))
+    print( A)
+    return A
+# 4== 4*3*2*1
+def factorial(num):
+    result=1
+    while num > 0 :
+        result=result* num
+        num=num -1
+    return result
+
+def add_numbers(*args):
+    result=0
+    for num in args:
+        result+=num
+    return result
+
+# function that if i give cash ksh3000
+def clean_numbers(word)
+    return result
+print(clean_numbers("ksh6000")) #6000
+
+print(add_numbers(2,5))
+print(add_numbers(2,5,7,1))
+#print(factorial(4))
+# use a function== call function
+
+#area_of_triangle(10,10,10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# volume_cylinder(10,10,3)
+# volume_cylinder(16,10,5)
+# volume_cylinder(5,10,2)
+# volume_cylinder(r=45, h=67, precision=2)# named params
+
+
+
+
+
+#area_circle(7)
+#area_circle(25)
+#area_circle(7.5654)
+
+
+
+#greet()
+#greet()
+#greet()
---
+++ functions_2.py
@@ -0,0 +1,11 @@
+from functions import area_of_triangle
+
+result=area_of_triangle(7,2,6)
+
+result_2=pow(5,3)
+
+math.sqrt(9)
+
+v=result *20
+print(pow(2,3))
+round( 9.677)# standard
